cb29fb06c3105ca3cac0165c5bfbf9e20b50af55
RELNO <arielnoyman@gmail.com>
2017-02-27 09:02:56 -0500

    Hello World!
    First Commit

---
+++ Assets/Resources/scripts/AgentsController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.AI;
+
+public class AgentsController : MonoBehaviour
+{
+
+	private GameObject spwanerParent;
+	private new List <GameObject> otherList;
+	private NavMeshAgent thisAgentNavMesh;
+	private TrailRenderer tr;
+	public float timeAtAmenity;
+
+	private Color thisColor;
+
+
+	void Start ()
+	{
+
+		spwanerParent = GameObject.Find ("Spawners");
+		otherList = spwanerParent.GetComponent<AgentsSpawner> ().AgentsList; 
+
+		thisColor = gameObject.GetComponent<Renderer> ().material.color;
+		tr = GetComponent<TrailRenderer> ();
+		tr.material = new Material (Shader.Find ("Sprites/Default"));
+
+		// A simple 2 color gradient with a fixed alpha of 1.0f.
+		float alpha = 0.5f;
+		Gradient gradient = new Gradient ();
+		gradient.SetKeys (
+			new GradientColorKey[] { new GradientColorKey (thisColor, 0.0f), new GradientColorKey (thisColor, 1.0f) },
+			new GradientAlphaKey[] { new GradientAlphaKey (alpha, 0f), new GradientAlphaKey (alpha / 100, 1f) }
+		);
+		tr.colorGradient = gradient;
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		thisAgentNavMesh = this.GetComponent<NavMeshAgent> ();
+
+//		if (other.gameObject.tag == "kill") {
+//			Destroy (gameObject);
+//			otherList.Remove (gameObject);
+//			//Debug.Log (otherList.Count);
+
+		if (other.gameObject.tag == "amenity") {
+			StartCoroutine (killAfterTime());
+
+
+		}
+
+//		} else if (other.gameObject.tag != this.gameObject.tag) {
+//			//draw nice things 
+//		}
+	}
+
+	IEnumerator killAfterTime ()
+	{
+		//Debug.Log ("amen");
+		thisAgentNavMesh.Stop();
+		yield return new WaitForSeconds (timeAtAmenity);
+		Destroy (gameObject);
+		otherList.Remove (gameObject);
+	}
+
+	
+	//	void Update ()
+	//	{
+	//		for (int i = 0; i < otherList.Count; i++) {
+	//			var thisagent = otherList [i];
+	//		}
+	//
+	//	}
+	//
+}
---
+++ Assets/Resources/scripts/AgentsSpawner.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class AgentsSpawner : MonoBehaviour
+{
+
+	//Vars here
+	public float delaySpwan = 1f;
+	public float startAgents = 500;
+	private GameObject agent;
+
+	public GameObject agentPrefab;
+	private UnityEngine.AI.NavMeshAgent agentNavMesh;
+
+
+	public GameObject spawnParent;
+	public GameObject targetParent;
+	private List <Transform> SpwanLocations;
+	private List <Transform> TargetLocations;
+
+	public List<GameObject> AgentsList;
+
+	void Awake ()
+	{
+		// go through all children of spawn parent and add them to a list 
+		SpwanLocations = spawnParent.GetComponentsInChildren<Transform> ().Skip (1).ToList ();
+		TargetLocations = targetParent.GetComponentsInChildren<Transform> ().Skip (1).ToList ();
+
+		for (int i = 0; i < startAgents; i++) {
+			spwanerMethod ();
+		}
+	}
+
+	IEnumerator Start ()
+	{
+
+		while (true) {
+
+			if (AgentsList.Count < startAgents * 2) {
+				yield return new WaitForSeconds (delaySpwan);
+				Debug.Log ("spwaning");
+				spwanerMethod (); //call creation method
+
+			} else {
+				yield return new WaitForSeconds (delaySpwan * 5);
+				Debug.Log ("too many agents");
+
+				spwanerMethod (); //call creation method
+
+			}
+				
+		}
+	}
+
+	void spwanerMethod ()
+	{
+		// creating the agent //
+		Transform randomSpwanLocation = SpwanLocations [Random.Range (0, SpwanLocations.Count)];
+		agent = GameObject.Instantiate 
+			(agentPrefab, randomSpwanLocation.position, randomSpwanLocation.rotation, transform) as GameObject; //make the agent
+		UnityEngine.AI.NavMeshAgent agentNavMesh = agent.GetComponent<UnityEngine.AI.NavMeshAgent> ();
+		AgentsList.Add (agent);
+
+		// Target alocation // 
+
+		var value = 100 * Random.value;
+		if (value < 30) { //spwan 
+
+			Transform randomTarget = TargetLocations [Random.Range (0, TargetLocations.Count / 3)];
+			agentNavMesh.SetDestination (randomTarget.position);
+			agentNavMesh.GetComponent<Renderer> ().material.color = Color.red;
+			agent.tag = "Rich"; 
+
+		} else if (value > 30 && value < 60) { //or to random targets 
+
+			Transform randomTarget = TargetLocations [Random.Range (TargetLocations.Count / 3, ((TargetLocations.Count * 60) / 100))];
+			agentNavMesh.SetDestination (randomTarget.position);
+			agentNavMesh.GetComponent<Renderer> ().material.color = Color.yellow;
+			agent.tag = "Medium";
+
+		} else if (value > 60 && value < 100) { //or to random targets 
+
+			Transform randomTarget = TargetLocations [Random.Range (((TargetLocations.Count * 60) / 100), TargetLocations.Count)];
+			agentNavMesh.SetDestination (randomTarget.position);
+			agentNavMesh.GetComponent<Renderer> ().material.color = Color.green;
+			agent.tag = "Poor";
+
+		}
+	}
+}
+	
