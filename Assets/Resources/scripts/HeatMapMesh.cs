9828c4f0762a39428815c6051d2c2abdf26090ed
RELNO <arielnoyman@gmail.com>
2017-03-29 15:55:49 +0800

    update

--- Assets/Resources/scripts/HeatMapMesh.cs
+++ Assets/Resources/scripts/HeatMapMesh.cs
@@ -10,6 +10,7 @@ public class HeatMapMesh: MonoBehaviour
 
 
 	public GameObject _cityIoObj;
+	public GameObject _reciever;
 	public GameObject _targetsParent;
 	public List <Transform> TargetsList;
 	public  List<GameObject> _heatMapPixels = new List<GameObject> ();
@@ -29,7 +30,9 @@ public class HeatMapMesh: MonoBehaviour
 
 	void Update ()
 	{
-		Mesh mesh = GetComponent<MeshFilter> ().mesh;
+		Mesh _recieverMesh = _reciever.GetComponent<MeshFilter> ().mesh;
+		Mesh _heatmapMesh = GetComponent<MeshFilter> ().mesh;
+
 
 			
 		foreach (var i in TargetsList) {
@@ -37,25 +40,35 @@ public class HeatMapMesh: MonoBehaviour
 			_agentsAtTarget = (_targetsVars._medium + _targetsVars._poor + _targetsVars._rich); //should show more specific response to types !!
 
 			if (Physics.Raycast (i.transform.position, Vector3.up, out _hitInfo, Mathf.Infinity)) {
+				//Debug.DrawRay (i.transform.position, _hitInfo.collider.transform.position);
+				//Debug.DrawRay (i.transform.position, new Vector3 (i.transform.position.x, _hitInfo.collider.transform.position.y , i.transform.position.z));
 
-				var MC = _hitInfo.collider as MeshCollider;
-				if (MC != null) {
+				var _meshCollider = _hitInfo.collider as MeshCollider;
+				if (_meshCollider != null) {
 					var index = _hitInfo.triangleIndex * 3;
-					var hit1 = mesh.vertices [mesh.triangles [index]];
+					var hit1 = _recieverMesh.vertices [_recieverMesh.triangles [index]];
 					//var hit2 = mesh.vertices [mesh.triangles [index + 1]];
 					//var hit3 = mesh.vertices [mesh.triangles [index + 2]];
 
-					Vector3[] vertices = mesh.vertices;
-					int[] triangles = mesh.triangles;
+					Vector3[] vertices = _recieverMesh.vertices;
+					int[] triangles = _recieverMesh.triangles;
 
 					int x = 0;
 					while (x < vertices.Length) {
-						if (vertices [x] == hit1 && Mathf.Sqrt( _agentsAtTarget) > 0)
-							vertices [x] = new Vector3 (hit1.x,  _agentsAtTarget,hit1.z);
+						if (vertices [x].x == hit1.x && vertices [x].z == hit1.z && _agentsAtTarget > 0)
+							//vertices [x] = new Vector3  (hit1.x, transform.localScale.x / _agentsAtTarget  , hit1.z);
+							vertices [x] =  Vector3.Lerp (new Vector3(hit1.x,hit1.y,hit1.z),
+								new Vector3 (hit1.x, transform.localScale.x / _agentsAtTarget,hit1.z), .1f);
+
 						x++;
 				
-						mesh.vertices = vertices;
-						mesh.RecalculateBounds ();
+
+						_recieverMesh.vertices = vertices;
+						_recieverMesh.RecalculateBounds ();
+
+						_heatmapMesh.vertices = vertices;
+						_heatmapMesh.RecalculateBounds ();
+
 					}
 				}
 			}
